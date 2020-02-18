8abb425d7e1faa084231a99492ad353d52912551
yeonnseok <ljinsk3@gmail.com>
2020-02-18 16:55:07 +0900

    요구사항 2 : unchecked Exception 에서 checked Exception으로 수정

--- src/main/java/coordinate/utils/InvalidFigureException.java
+++ src/main/java/coordinate/utils/InvalidFigureException.java
@@ -1,6 +1,6 @@
 package coordinate.utils;
 
-public class InvalidFigureException extends RuntimeException {
+public class InvalidFigureException extends Exception {
     public InvalidFigureException(String message) {
         super(message);
     }
